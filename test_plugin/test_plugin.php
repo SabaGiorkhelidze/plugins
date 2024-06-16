9b78eb7b1110999378ce4296018e2aa82ba7ee46
SabaGiorkhelidze <saba.giorkhelidze.1@btu.edu.ge>
2024-06-16 19:22:08 +0400

    added section and tested it in db

--- test_plugin/test_plugin.php
+++ test_plugin/test_plugin.php
@@ -22,37 +22,84 @@ Author URI: github.com/SabaGiorkhelidze
 
 
 
-
-
-add_action('admin_menu', "WordCountPlugginToSettingsLink");
-
-
-
-function WordCountPlugginToSettingsLink()
-{
-    # adds new link to a settings dropwdown
-    /* args(1,2,3,4,5) 
+# adds new link to a settings dropwdown
+#    add_options_page
+/* args(1,2,3,4,5) 
         1 - title of the doc page (<title>My web app</title> or something like that)
         2 - title of the menu (which will be clicked by user when hovering on settings menu)
         3 - Permissions, capabilities (who will see this)
         4 - slug, shortname (we use it for end of an url)
         5 - function that outputs html content
+*/
 
+class WordCountAndTimePlugin
+{
+    function __construct()
+    {
+        add_action('admin_menu', array($this, "AdminPagePluggin"));
+        add_action("admin_init", array($this, 'settings'));
+    }
 
+    function settings()
+    {
+        add_settings_section("wordCountPluginSection", null, null, "word count settings page");
+        // (section name, title of section, content on top of the section, page slug)
+
+        add_settings_field(
+            "wordCountPluginLocation", // 1. ID - unique identifier for the setting field
+            "Display Location",        // 2. Title - label text displayed next to the field
+            array($this, 'locationHTML'), // 3. Callback Function - method that outputs the field's HTML
+            "word count settings page", // 4. Page - slug of the settings page where the field appears
+            "wordCountPluginSection"   // 5. Section - ID of the section within the settings page
+        );
+        register_setting("wordCountPlugin", "wordCountPluginLocation", array("sanitize_callback" => "sanitize_text_field", "default" => '0'));
+        /*
+        args
+        1 - name of group it belongs to
+        2 - actual name for setting
+        3 - values also includes default value if none is selected
         */
+    }
 
-    add_options_page(
-        "Word Count Settings",
-        "Word Count",
-        "manage_options",
-        "word count settings page",
-        "settingsPageHTML"
-    );
+
+    function locationHTML()
+    { ?>
+        <select name="wordCountPluginLocation">
+            <option value="0">beginning of post</option>
+            <option value="1">end of post</option>
+        </select>
+    <?php
+
+    }
+
+    function AdminPagePluggin()
+    {
+        add_options_page(
+            "Word Count Settings",
+            "Word Count",
+            "manage_options",
+            "word count settings page",
+            array($this, "PageHTML")
+        );
+    }
+
+    function PageHTML()
+    { ?>
+        <div class="wrap">
+            <h1>Word Count Settings</h1>
+            <form action="options.php" method="POST">
+                <?php 
+                settings_fields("wordCountPlugin");
+                    do_settings_sections('word count settings page');
+                    submit_button();
+                ?>
+            </form>
+        </div>
+<?php }
 }
 
-function settingsPageHTML()
-{ ?>
-Hello from new plugin    
-<?php }
+
+$WordCountAndTimePlugin = new WordCountAndTimePlugin();
+
 
 ?>
