f60266cfbacb2bfaae1a77f441c2a0ce2f4acace
SabaGiorkhelidze <saba.giorkhelidze.1@btu.edu.ge>
2024-06-16 18:30:10 +0400

    added settings Page for word count

--- test_plugin/test_plugin.php
+++ test_plugin/test_plugin.php
@@ -10,14 +10,49 @@ Author URI: github.com/SabaGiorkhelidze
 
 */
 
-function addSentanceToEnd($content)
+// function addSentanceToEnd($content)
+// {
+//     if (is_page() && is_main_query()) {  #avoids to add this line to all the pages and posts and content like navbar sidebar etc. 
+//         return $content . ' <p>This was added by me: Saba</p>';
+//     }
+//     return $content;
+// }
+
+// add_filter('the_content', 'addSentanceToEnd')
+
+
+
+
+
+add_action('admin_menu', "WordCountPlugginToSettingsLink");
+
+
+
+function WordCountPlugginToSettingsLink()
 {
-    if (is_page() && is_main_query()) {  #avoids to add this line to all the pages and posts and content like navbar sidebar etc. 
-        return $content . ' <p>This was added by me: Saba</p>';
-    }
-    return $content;
+    # adds new link to a settings dropwdown
+    /* args(1,2,3,4,5) 
+        1 - title of the doc page (<title>My web app</title> or something like that)
+        2 - title of the menu (which will be clicked by user when hovering on settings menu)
+        3 - Permissions, capabilities (who will see this)
+        4 - slug, shortname (we use it for end of an url)
+        5 - function that outputs html content
+
+
+        */
+
+    add_options_page(
+        "Word Count Settings",
+        "Word Count",
+        "manage_options",
+        "word count settings page",
+        "settingsPageHTML"
+    );
 }
 
-add_filter('the_content', 'addSentanceToEnd')
+function settingsPageHTML()
+{ ?>
+Hello from new plugin    
+<?php }
 
 ?>
